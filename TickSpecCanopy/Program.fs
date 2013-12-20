8c7a292df2a772ef52a0b4a2e10caeb6cff0ade9
Jon Canning <jon.canning@gmail.com>
2013-12-20 22:11:03 +0000

    Introduced FileNotFound exception

--- TickSpecCanopy/Program.fs
+++ TickSpecCanopy/Program.fs
@@ -6,12 +6,10 @@ open TickSpec
 let assembly = Assembly.GetExecutingAssembly()
 let definitions = StepDefinitions(assembly)
 
-let executeDefinition source = 
-    let s = assembly.GetManifestResourceStream(source)
-    match s with
-    | null -> ()
-    | _ -> definitions.Execute(source, s)
-
-let executeFeature feature = [ feature ] |> Seq.iter executeDefinition
+let executeFeature featureFile = 
+    let stream = assembly.GetManifestResourceStream(featureFile)
+    match stream with
+    | null -> raise (System.IO.FileNotFoundException(featureFile))
+    | _ -> definitions.Execute(featureFile, stream)
 
 executeFeature "mutantCamelFeature.txt"

d7da6da4ebdd2de36f89002a7478b6ec550451a3
Jon Canning <jon.canning@gmail.com>
2013-12-20 21:53:24 +0000

    Initial commit

---
+++ TickSpecCanopy/Program.fs
@@ -0,0 +1,17 @@
+﻿module Program
+
+open System.Reflection
+open TickSpec
+
+let assembly = Assembly.GetExecutingAssembly()
+let definitions = StepDefinitions(assembly)
+
+let executeDefinition source = 
+    let s = assembly.GetManifestResourceStream(source)
+    match s with
+    | null -> ()
+    | _ -> definitions.Execute(source, s)
+
+let executeFeature feature = [ feature ] |> Seq.iter executeDefinition
+
+executeFeature "mutantCamelFeature.txt"
---
+++ TickSpecCanopy/mutantCamelFeature.fs
@@ -0,0 +1,20 @@
+﻿module featureStepDefinitions
+
+open TickSpec
+open canopy
+
+[<Given>]
+let ``I visit (.*)`` s = 
+    start firefox
+    url s
+
+[<Given>]
+let ``I type (.*) into the input box with the id (.*)`` s id = id << s
+
+[<When>]
+let ``I press enter`` () = press enter
+
+[<Then>]
+let ``the first result should have the title "(.*)"`` s = 
+    "h3.r a" == s
+    quit()
